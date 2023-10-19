1174ca1b58b5113ae5e4bc9724c4d5bcf7a02115
Victor Golovin <rinoally@gmail.com>
2023-10-19 20:23:34 +0300

    start react-api, useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { 
+  useState,
+  useEffect 
+} from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [users, setUsers] = useState([]);
+
+  console.log('components rendered')
+
+  useEffect(() => { // useEffect() нужен для того чтобы выполнить запрос один раз, без него у нас постоянно будет вызываться функция fetch
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      // setUsers(data)
+    });
+  
+    // .catch(error => console.log(error));
+  });
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      { JSON.stringify(users) }
     </>
   )
 }
