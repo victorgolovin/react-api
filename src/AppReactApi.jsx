d95c8a57d47fe61653a72e99584bad2c72668b19
Victor Golovin <rinoally@gmail.com>
2023-10-20 22:16:15 +0300

    add useState Error

--- src/AppReactApi.jsx
+++ src/AppReactApi.jsx
@@ -6,23 +6,43 @@ import {
 import ReactUseEffectsArray from './ReactUseEffectsArray';
 
 const AppReactApi = () => {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState(null);
+  const [isLoading, setIsLoading] = useState(false); // создаем стейт для отображения "загрузка" на экране когда идет обновление страницы и ожидания ответа от бэкенда
+  const [isError, setIsError] = useState(false); // как себя будет вести интерфейс если у нас ошибка в бекенде
 
   console.log('components rendered')
 
   useEffect(() => { // useEffect() нужен для того чтобы выполнить запрос один раз, без него у нас постоянно будет вызываться функция fetch
+    setIsLoading(true) // идет загрузка
+    setIsError(false) // ошибки нет
+
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
+      .then(response => {
+        if (!response.ok) { // делаем проверку респонса через ключ ок
+          console.log('с запросом что-то не так', response);
+          throw new Error('ошибка в запросе') // благодаря throw new Error у нас выполнится код ниже (catch error)
+        }
+
+        return response.json()
+      })
       .then(data => {
         console.log(data);
-        setUsers(data)
-    });
-    // .catch(error => console.log(error));
+        setUsers([data]);
+        setIsLoading(false); // конец загрузки
+                        
+    })
+       .catch(error => { // данный кетч нужен для ошибки при запросе
+        console.log(error)
+        setIsLoading(false) // сообщение об ошибки не выводим на экран т.к уже получили сообщее об ошибке
+        setIsError(true) // ошибки есть
+      });
   }, []); // пусктой массив в useEffect говорит ему чтобы он выполнился один раз внутри компонента
 
   return (
     <>
-      { JSON.stringify(users) }
+      { isError ? 'ошибка запроса' : '' }
+      { isLoading ? 'Загрузка' : '' }
+      { users ? JSON.stringify(users) : ''}
 
       <p>-------------------NEXT LESSON------------------</p>
 
--- src/ReactUseEffectsArray.jsx
+++ src/ReactUseEffectsArray.jsx
@@ -12,12 +12,28 @@ import {
         console.log('вызов useEffects')
         localStorage.setItem('count', count);
     }, [count]); // добавив в массив count мы говорим чтобы localStorage засинхронизировался с помощью useEffect
+
+    const handleButtonClick = () => {
+        setCount(count + 1)
+        localStorage.setItem('count', count);
+    }
     
     return (
       <div>
         {count}
+
+        <p>---------Если нужно синхронизовать------------- </p>
+
         <div>
             <button onClick={() => setCount(count + 1)}>
+                Count++
+            </button>
+        </div>
+
+        <p>---------Если нужно чтобы после нажатия сразу изменялись действия------------- </p>
+
+        <div>
+            <button onClick={handleButtonClick}>
                 Count++
             </button>
         </div>
